999c367e736ab40e6b5515a5966a2a4ddebb6646
Alexandr Kolganov <gengizar@yandex.ru>
2023-01-04 11:27:35 +0500

    add skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
+import Skeleton from './components/PizzaBlock/Skeleton';
 import './scss/app.scss';
-import pizzas from './assets/pizzas.json';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://63b464300f49ecf50889ba66.mockapi.io/items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setPizzas(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -17,9 +32,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
+              : pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
