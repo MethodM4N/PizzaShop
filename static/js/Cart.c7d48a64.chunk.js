7de3dc24b927acf9825f9ea6e45a06dff165157b
Alexandr Kolganov <gengizar@yandex.ru>
2023-03-15 11:19:01 +0500

    Updates

---
+++ static/js/Cart.c7d48a64.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpizza_shop=self.webpackChunkpizza_shop||[]).push([[72],{8303:function(e,n,s){s.r(n);s(2791);var u=s(4072),a=s(184);n.default=function(){return(0,a.jsx)(a.Fragment,{children:(0,a.jsx)(u.iv,{})})}}}]);
+//# sourceMappingURL=Cart.c7d48a64.chunk.js.map
